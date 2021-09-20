1f16b55e458e32e5dc9a1d84117636bbc69f7fe1
CharlesMWolfe <cmwolfe.dev@gmail.com>
2021-09-20 03:20:00 -0500

    Commented out the print messages line. Comments saved to file correctly

--- reddit.py
+++ reddit.py
@@ -16,7 +16,8 @@ from secrets import reddit_client_secret, reddit_client_id, reddit_user_agent
 # -------------------------------------------------------------------------- #
 
 # now that I have imported everything, CREATE A REDDIT INSTANCE:
-reddit = praw.Reddit(client_id=reddit_client_id, client_secret=reddit_client_secret, user_agent=reddit_user_agent)
+reddit = praw.Reddit(client_id=reddit_client_id,
+                     client_secret=reddit_client_secret, user_agent=reddit_user_agent)
 
 # create list of subreddits I want to extract post and comment data from each day:
 subreddits = ["wallstreetbets", "StocksAndTrading", "Daytrading", "StockMarket", "CryptoCurrency",
@@ -41,14 +42,17 @@ def return_subreddit_df(subreddit="all", limit=25):
     # empty list to insert data to:
     posts = []
     # variable for data
-    top_posts = reddit.subreddit(subreddit).hot(limit=limit) # limit and subreddit params
+    top_posts = reddit.subreddit(subreddit).hot(
+        limit=limit)  # limit and subreddit params
 
     # FOR loop to append data to posts
     for post in top_posts:
-        posts.append([post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, post.created])
+        posts.append([post.title, post.score, post.id, post.subreddit,
+                     post.url, post.num_comments, post.selftext, post.created])
 
     # Create df
-    df = pd.DataFrame(posts, columns=["title","score", "id", "subreddit", "url", "comments","selftext", "created"])
+    df = pd.DataFrame(posts, columns=[
+                      "title", "score", "id", "subreddit", "url", "comments", "selftext", "created"])
     return df
 
 
@@ -90,8 +94,10 @@ def choose_subreddit(subreddit_list):
     subreddit = subreddit_list[idx]
     return subreddit
 
+
 # CREATE PRAW INSTANCE
-reddit = praw.Reddit(client_id=reddit_client_id, client_secret=reddit_client_secret, user_agent=reddit_user_agent)
+reddit = praw.Reddit(client_id=reddit_client_id,
+                     client_secret=reddit_client_secret, user_agent=reddit_user_agent)
 
 # HAVE USER CHOOSE THE SUBREDDIT TO GET COMMENT DATA FOR
 subreddit = choose_subreddit(subreddits)
@@ -105,7 +111,7 @@ df.to_csv(post_path, sep='|')
 
 # PASS DF INTO FUNC TO RETRIEVE POST COMMENTS
 comments = return_comments_for(df["id"])
-print(comments) # make sure it is working then comment this out or delete
+# print(comments) # make sure it is working then comment this out or delete
 comment_df = pd.DataFrame(comments, columns=["Comments"])
 # write data to CSV file for now. Save in Data folder
 comment_path = f"data/{subreddit}_comments.csv"
